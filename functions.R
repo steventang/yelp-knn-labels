c3191802e3c64bc3cd36fe6005cd72c7c288e78f
Steven Tang <stang12290@gmail.com>
2017-03-06 23:14:42 -0800

    initial commit

---
+++ functions.R
@@ -0,0 +1,274 @@
+# Returns knn_matrix
+find_nearest_neighbors <- function(dfm, test_dfm, train_dfm, num_neighbors, remove_test = TRUE) {
+  # Turn our entire dfm into a tf matrix, keeping original dfm in tact
+  print("Calculating tfidf scores...")
+  dfm_tf <- tf(dfm, scheme = "prop")
+  train_idf <- docfreq(train_dfm, scheme = "inverse")
+  # Because our training set will have fewer features than our entire dfm, set idf of missing features to 0 (else returns Inf idf score)
+  train_idf[!is.finite(train_idf)] <- 0
+  # Our entire dfm is now tfidf-scored using our training idf values
+  dfm_tfidf <- dfm_weight(dfm_tf, weights = train_idf)
+  
+  # Find cosine distances of our test docs compared to our entire dfmsparse. Note this matrix still contains similarities to our test docs
+  print("Calculating cosine similarity matrix...")
+  simil_matrix <- similarity(dfm_tfidf, docnames(test_dfm), margin = "documents", method = "cosine") # Make global, we access this matrix elsewhere
+  
+  row_i <- 1
+  knn_matrix <- matrix(NA, nrow = length(docnames(test_dfm)), ncol = num_neighbors + 1) # +1 since first col is name of our doc
+  
+  # Construct new matrix of doc and all top neighbor similarities to each doc
+  # knn_matrix is a matrix where first column represents our test doc, and subsequent columns in the row are its nearest neighbors
+  print("Creating nearest neighbors matrix...")
+  for (doc in names(simil_matrix)){
+    if (remove_test) {
+      # Remove all test values from our similarity matrix so we don't use them as neighbors. We only want to compare test docs to training neighbors
+      # In calculating knn for just our training set, we will pass in our test and training dfm as the same dfm, so we'd keep nothing if we removed all test values
+      sub <- subset(simil_matrix[[doc]], !(names(simil_matrix[[doc]]) %in% docnames(test_dfm)))
+    }
+    else {
+      sub <- simil_matrix[[doc]]
+    }
+    knn_matrix[row_i, ] <- c(doc, names(sub[1:num_neighbors]))
+    row_i <- row_i + 1
+  }
+  return(knn_matrix)
+}
+
+### REFACTORED VERSION WITH COLNAMES ###
+classify_neighbors <- function(knn_matrix, tags) {
+  count_matrix <- matrix(0, nrow = nrow(knn_matrix), ncol = (length(tags) + 1))
+  colnames(count_matrix) <- c("docname", tags)
+  
+  # Presumes that our corpus is labeled with docvars corresponding to our tags accessed thru corpus$documents[[tag]]
+  for (row_i in (1:nrow(knn_matrix))) {
+    count_matrix[row_i, 1] <- knn_matrix[row_i, 1] # Keep name of doc in first col
+    for (tag in tags) {
+      # For all of a docs neighbors, examine whether any each of them have this tag.
+      # The length of the list should be # neighbors that have this tag
+      count_matrix[row_i, tag] <- sum(corpus_subset(corpus, docnames(corpus) %in% knn_matrix[row_i, -1])$documents[[tag]]) # Sum() counts num TRUE
+    }
+  }
+  return(count_matrix)
+}
+
+
+# Prior probability of a doc having a tag in our training set
+mlknn_priors <- function(train_dfm, tags) {
+  priors_matrix <- list()
+  for (tag in tags) {
+    # Count all documents in training set with tag
+    doc_count <- sum(corpus_subset(corpus, docnames(corpus) %in% docnames(train_dfm))$documents[[tag]])
+    priors_matrix[tag] <- (1 + doc_count) / (2 + ndoc(train_dfm))
+  }
+  return(priors_matrix)
+}
+
+mlknn_posteriors <- function(train_dfm, count_matrix, tags, K) {
+  # Matrix values are posterior probability that exactly j neighbors (row) either have/don't have label L (col)
+  posteriors_has_label_matrix <- matrix(0, nrow = K + 1, ncol = length(tags) + 1)
+  colnames(posteriors_has_label_matrix) <- c("num_neighbors", tags)
+  posteriors_no_label_matrix <- matrix(0, nrow = K + 1, ncol = length(tags) + 1)
+  colnames(posteriors_no_label_matrix) <- c("num_neighbors", tags)
+  # Create a training count_matrix to get tags of closest neighbors of everyone in our training docs.
+  count_matrix <- classify_neighbors(find_nearest_neighbors(train_dfm, train_dfm, train_dfm, K, remove_test = FALSE), tags)
+  for (tag in tags) {
+    cat("Calculating posteriors for tag", tag, " ")
+    # c[j] is the number of docs in training set that have some label L while exactly j of its KNN's also have label L
+    c_match <- list(rep(0, K + 1))[[1]] # K+1 since we also look at 0 neighbor casea
+    c_prime <- list(rep(0, K + 1))[[1]]
+    
+    for (row_i in (1:ndoc(train_dfm))) {
+      delta <- as.integer(count_matrix[[row_i, tag]])
+      # If the document is labeled with tag...
+      # if (corpus_subset(corpus, docnames(corpus) == count_matrix[[row_i, 'docname']])$documents[[tag]] == 1) {
+      if (corpus[[count_matrix[[row_i, 'docname']], tag]] == 1) { # REFACTORED VER OF ABOVE
+        c_match[delta + 1] <- c_match[delta + 1] + 1 # delta+1 because our R array indices start at 1. c[1] is 0 neighbor case
+      }
+      else {
+        c_prime[delta + 1] <- c_prime[delta + 1] + 1
+      }
+    }
+    
+    for (j in 0:K) {
+      posteriors_has_label_matrix[j + 1, "num_neighbors"] <- j
+      posteriors_no_label_matrix[j + 1, "num_neighbors"] <- j
+      posteriors_has_label_matrix[j + 1, tag] <- (1 + c_match[j + 1]) / (K + 1 + sum(c_match)) # Laplace smoothing
+      posteriors_no_label_matrix[j + 1, tag] <- (1 + c_prime[j + 1]) / (K + 1 + sum(c_prime))
+    }
+  }
+  return(list(has_label = posteriors_has_label_matrix, 
+              no_label = posteriors_no_label_matrix))
+}
+
+# Pass in test count_matrix, and priors/posteriors from training set
+# Prediction matrix where rows are docs, cols are tags
+mlknn_predict <- function(count_matrix, priors, posteriors, tags, K) {
+  predict_matrix <- matrix(0, nrow = nrow(count_matrix), ncol = length(tags) + 1)
+  colnames(predict_matrix) <- c("docname", tags)
+  for (row_i in (1:nrow(count_matrix))) {
+    predict_matrix[row_i, 1] <- count_matrix[[row_i, 1]]
+    for (tag in tags) {
+      # Posterior is calculated by seeing how many neighbors this doc has have for this tag, then seeing what that corresponds to in our posterior matrix
+      prob_has_label <- priors[[tag]] * posteriors$has_label[[which(posteriors$has_label[, 'num_neighbors'] == as.integer(count_matrix[[row_i, tag]])), tag]]
+      prob_no_label <- priors[[tag]] * posteriors$no_label[[which(posteriors$no_label[, 'num_neighbors'] == as.integer(count_matrix[[row_i, tag]])), tag]]
+      if (which.max(c(prob_has_label, prob_no_label)) == 1) {
+        predict_matrix[row_i, tag] <- 1
+      }
+      else {
+        predict_matrix[row_i, tag] <- 0
+      }
+    }
+  }
+  return(predict_matrix)
+}
+
+# If we are to measure how useful as a recommendation tool to Yelp, ranking labels is useful
+# Rank matrix returns the probability of the doc having the tag in each position
+rank_labels <- function(count_matrix, priors, posteriors, tags, K) {
+  rank_matrix <- matrix(0, nrow = nrow(count_matrix), ncol = length(tags) + 1)
+  colnames(rank_matrix) <- c("docname", tags)
+  for (row_i in (1:nrow(count_matrix))) {
+    rank_matrix[row_i, 1] <- count_matrix[[row_i, 1]]
+    for (tag in tags) {
+      # Posterior is calculated by seeing how many neighbors this doc has have for this tag, then seeing what that corresponds to in our posterior matrix
+      prob_has_label <- priors[[tag]] * posteriors$has_label[[which(posteriors$has_label[, 'num_neighbors'] == as.integer(count_matrix[[row_i, tag]])), tag]]
+      prob_no_label <- priors[[tag]] * posteriors$no_label[[which(posteriors$no_label[, 'num_neighbors'] == as.integer(count_matrix[[row_i, tag]])), tag]]
+      rank_matrix[row_i, tag] <- prob_has_label / (prob_has_label + prob_no_label)
+    }
+  }
+  return(rank_matrix)
+}
+
+
+hamming_loss <- function(mlknn_predictions, tags) {
+  num_labels <- length(tags)
+  loss <- 0
+  for (row_i in (1:nrow(mlknn_predictions))) {
+    missclassifications <- 0
+    prediction <- mlknn_predictions[row_i, ]
+    truth <- corpus[[mlknn_predictions[[row_i, 'docname']], tags]]
+    for (tag in tags) {
+      if (as.integer(prediction[tag]) != truth[[tag]]) {
+        missclassifications <- missclassifications + 1
+      }
+    }
+    loss <- loss + (missclassifications / num_labels)
+  }
+  return(loss / nrow(mlknn_predictions))
+}
+
+# Accuracy doesn't account for false positive misclassifications, ie we say 1 but truth is 0
+# It is total number of times our predicted 1 equalled a truth 1 divided by total number of 1s
+accuracy <-function(mlknn_predictions, tags) {
+  sum_accuracy <- 0
+  for(row_i in (1:nrow(mlknn_predictions))) {
+    prediction <- mlknn_predictions[row_i, ]
+    truth <- corpus[[mlknn_predictions[[row_i, 'docname']], tags]]
+    correct <- 0
+    total <- 0
+    for(tag in tags) {
+      # If we correctly predicted a labelling
+      if(as.integer(prediction[tag]) == 1 & truth[[tag]] == 1) {
+        correct <- correct + 1
+      }
+      # Total number of labels positive labelings in both truth and predictions
+      if(as.integer(prediction[tag]) == 1 | truth[[tag]] == 1) {
+        total <- total + 1
+      }
+    }
+    if(total != 0) {
+      sum_accuracy <- sum_accuracy + (correct / total)
+    }	
+  }
+  return(sum_accuracy / nrow(mlknn_predictions))
+}
+
+# How many times top predicted label is not in a set of true labels in an instance
+one_error <- function(rank_matrix) {
+  error <- 0
+  for(row_i in (1:nrow(rank_matrix))) {
+    # Find name of top label
+    top_label <- names(which.max(rank_matrix[row_i, -1]))
+    # Check whether true observation does not have our top label
+    if(corpus[[rank_matrix[[row_i, 'docname']], top_label]] == 0) {
+      error <- error + 1
+    }
+    return(error / nrow(rank_matrix))
+  }
+}
+
+# Also useful - how far do we go down ranking list before finding all correct label on average
+coverage <- function(rank_matrix, tags) {
+  total <- 0
+  for(row_i in (1:nrow(rank_matrix))) {
+    # Find label names in our truth
+    truth <- corpus[[rank_matrix[[row_i, 'docname']], tags]]
+    true_labels <- names(truth[grep(1, truth)]) # Find position of 1s to get true label names
+    
+    # Find an ordered list of predicted label rankings
+    ranked_labels <-  names(sort(rank_matrix[row_i, -1], decreasing = TRUE))
+    position <- 1
+    
+    # Iterate over ranked label list until we find all true labels, tracking position 
+    while(length(true_labels) > 0) {
+      if(ranked_labels[position] %in% true_labels) {
+        # Find position of the label and delete it
+        true_labels <- true_labels[-match(ranked_labels[position], true_labels)]
+      }
+      position <- position + 1
+    }
+    total <- total + position - 1
+  }
+  return(total / nrow(rank_matrix))
+}
+
+
+# Returns our CV Splits matrix with errors in a new col 
+cv_error <- function(dfm, cv_splits, tags, K) {
+  errors <- matrix(0, ncol = 4, nrow = nrow(cv_splits)  + 1)
+  colnames(errors) <- c("hamming", "accuracy", "one-error", "coverage")
+  for(row_i  in (1:nrow(cv_splits))) {
+    
+    print(cat("Performing cross validation for fold ", row_i, " *********************"))
+    test_indicies <- cv_splits[[row_i ,1]]
+    train_indicies <- cv_splits[[row_i ,2]]
+    test <- dfm[test_indicies, ]
+    train <- dfm[train_indicies, ]
+    
+    print("Finding neighbors of our test set (knn_matrix)...")
+    knn_matrix <- find_nearest_neighbors(dfm, test, train, K, remove_test = TRUE)
+    
+    print("Classifying neighbors (count_matrix)...")
+    count_matrix <- classify_neighbors(knn_matrix, tags)
+    
+    print ("Calculating priors...")
+    priors <- mlknn_priors(train, tags)
+    
+    print("Calculating posteriors...")
+    posteriors <- mlknn_posteriors(train, count_matrix, tags, K)
+    
+    print("Making ML-KNN predictions...")
+    predictions <- mlknn_predict(count_matrix, priors, posteriors, tags, K)
+    
+    print("Calculating label rankings")
+    ranks <- rank_labels(count_matrix, priors, posteriors, tags, K)
+    
+    print("Calculating hamming loss...")
+    errors[row_i, 1] <- hamming_loss(predictions, tags)
+    
+    print("Calculating accuracy...")
+    errors[row_i, 2] <- accuracy(predictions, tags)
+    
+    print("Calculating one-error...")
+    errors[row_i, 3] <- one_error(ranks)
+    
+    print("Calculating coverage...")
+    errors[row_i, 4] <- coverage(ranks, tags)
+  }
+  
+  for (type in colnames(errors)) {
+    errors[nrow(cv_splits) + 1, type] <- mean(errors[1:nrow(cv_splits), type])
+  }
+  return(errors)
+  # return(errors)
+}
---
+++ preprocessing.R
@@ -0,0 +1,115 @@
+library(dplyr)
+library(stringr)
+library(jsonlite)
+library(tidyr)
+library(ggplot2)
+
+setwd("~/Yelp")
+
+#convert json file to df
+print("Converting business data json to data frame...")
+df_bus <- stream_in(file("yelp_businesses.json"))
+
+# convert to tibble for better readbility
+df_bus <- as_data_frame(df_bus)
+
+# Detect whether hipster true: true is part of attributes string
+print("Adding label columns to business data frame...")
+df_bus <- df_bus %>% mutate(hipster = str_detect(attributes, "'hipster': True") * 1)
+df_bus <- df_bus %>% mutate(trendy = str_detect(attributes, "'trendy': True") * 1)
+df_bus <- df_bus %>% mutate(classy = str_detect(attributes, "'classy': True") * 1)
+df_bus <- df_bus %>% mutate(divey = str_detect(attributes, "'divey': True") * 1)
+df_bus <- df_bus %>% mutate(touristy = str_detect(attributes, "'touristy': True") * 1)
+df_bus <- df_bus %>% mutate(casual = str_detect(attributes, "'casual': True") * 1)
+df_bus <- df_bus %>% mutate(intimate = str_detect(attributes, "'intimate': True") * 1)
+df_bus <- df_bus %>% mutate(romantic = str_detect(attributes, "'romantic': True") * 1)
+df_bus <- df_bus %>% mutate(upscale = str_detect(attributes, "'upscale': True") * 1)
+
+df_bus <- df_bus %>% mutate(dessert = str_detect(attributes, "'dessert': True") * 1)
+df_bus <- df_bus %>% mutate(latenight = str_detect(attributes, "'latenight': True") * 1)
+df_bus <- df_bus %>% mutate(lunch = str_detect(attributes, "'lunch': True") * 1)
+df_bus <- df_bus %>% mutate(dinner = str_detect(attributes, "'dinner': True") * 1)
+df_bus <- df_bus %>% mutate(breakfast = str_detect(attributes, "'breakfast': True") * 1)
+df_bus <- df_bus %>% mutate(brunch = str_detect(attributes, "'brunch': True") * 1)
+
+# Read in reviews data
+print("Converting reviews json to data frame...")
+df_rev <- stream_in(file("yelp_reviews.json"))
+df_rev <- as_data_frame(df_rev)
+
+# Left join business first because we filtered to only restaurants ealier and we only want business ID's that are still in df_bus
+print("Joining business and review data by business id...")
+data <- df_bus %>% left_join(df_rev, by="business_id")
+
+#Create rows that indicate whether it has a Restaurant/Nightlife category. These words are unique to the category so it's okay to just detect the string, though we may want to find exact match in future if data changes.
+print("Filtering for only Resaturant/Nightlife/Food categories and US states...")
+data <- data %>% mutate(restaurant = str_detect(categories, "Restaurant")) 
+data <- data %>% mutate(nightlife = str_detect(categories, "Nightlife"))
+# Create row that indicates whether it has a Food category. We use regex and gsub to detect an exact "Food" match with no other things, e.g. "Fast Food"
+data <- data %>% mutate(food = str_detect(gsub(" ", "", categories, fixed = TRUE), "\\bFood\\b"))
+data <- data %>% filter(restaurant == TRUE | food == TRUE | nightlife == TRUE)
+
+# For labelling problem we keep all US states
+state_keeps = c("AZ", "NC", "NV", "OH", "WI", "PA", "IL")
+data <- data %>% filter(state %in% state_keeps)
+
+# Keeping only relevant rows
+print("Keeping only relevant rows...")
+keeps <- c("business_id", "name", "city", "state", "review_count", "hipster", "trendy", "classy", "divey", "touristy", "casual", "intimate", "romantic", "upscale", "dessert", "latenight", "lunch", "dinner", "breakfast", "brunch", "text", "restaurant", "nightlife", "food")
+data <- data[keeps]
+
+# Group by business ID and consolidate data for a single business. Aggregates all reviews into one data point, one doc
+print("Collapsing all reviews and aggregate data by business...")
+data <- data %>% group_by(business_id) %>% summarize(name=first(name), review_count=mean(review_count), state=first(state), city=first(city), hipster=first(hipster), trendy=first(trendy), classy=first(classy), divey=first(divey), touristy=first(touristy), casual=first(casual), intimate=first(intimate), romantic=first(romantic), upscale=first(upscale), dessert=first(dessert), latenight=first(latenight), lunch=first(lunch), dinner=first(dinner), brunch=first(brunch), breakfast=first(breakfast), restaurant=first(restaurant), food=first(food), nightlife=first(nightlife), text=paste(text, collapse=" "))
+
+# Only keep businesses that have more than 20 reviews
+data <- data %>% filter(review_count > 19)
+
+# Only keep data that is labeled
+# Split into goodfor and ambience datasets to be modelled separately
+data_goodfor <- data %>% filter(dessert+latenight+lunch+dinner+brunch+breakfast > 0)
+
+data_ambience <- data %>% filter(hipster+trendy+classy+divey+touristy+casual+intimate+romantic+upscale > 0)
+
+# Create goodfor corpus and dfm
+
+corpus_goodfor <- corpus(data_goodfor$text)
+
+docvars(corpus_goodfor, "business_id") <- data_goodfor$business_id
+docvars(corpus_goodfor, "dessert") <- data_goodfor$dessert
+docvars(corpus_goodfor, "latenight") <- data_goodfor$latenight
+docvars(corpus_goodfor, "lunch") <- data_goodfor$lunch
+docvars(corpus_goodfor, "dinner") <- data_goodfor$dinner
+docvars(corpus_goodfor, "brunch") <- data_goodfor$brunch
+docvars(corpus_goodfor, "breakfast") <- data_goodfor$breakfast
+
+tags_goodfor <- c("dessert", "latenight", "lunch", "dinner", "brunch", "breakfast")
+
+dfm_goodfor <- dfm(corpus_goodfor, remove = stopwords("english"), stem = TRUE, removePunct = TRUE)
+dfm_goodfor <- dfm_trim(dfm_goodfor, sparsity = .9)
+
+# Entire data corpus and dfm
+
+corpus <- corpus(data$text)
+
+docvars(corpus, "business_id") <- data$business_id
+docvars(corpus, "hipster") <- data$hipster
+docvars(corpus, "trendy") <- data$trendy
+docvars(corpus, "classy") <- data$classy
+docvars(corpus, "touristy") <- data$touristy
+docvars(corpus, "divey") <- data$divey
+docvars(corpus, "casual") <- data$casual
+docvars(corpus, "intimate") <- data$intimate
+docvars(corpus, "romantic") <- data$romantic
+docvars(corpus, "upscale") <- data$upscale
+docvars(corpus, "dessert") <- data$dessert
+docvars(corpus, "latenight") <- data$latenight
+docvars(corpus, "lunch") <- data$lunch
+docvars(corpus, "dinner") <- data$dinner
+docvars(corpus, "brunch") <- data$brunch
+docvars(corpus, "breakfast") <- data$breakfast
+
+tags <- c("hipster", "trendy", "classy", "divey", "touristy", "casual", "intimate", "romantic", "upscale", "dessert", "latenight", "lunch", "dinner", "brunch", "breakfast")
+
+dfm <- dfm(corpus, remove = stopwords("english"), stem = TRUE, removePunct = TRUE)
+dfm <- dfm_trim(dfm, sparsity = .9)
